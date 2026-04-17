eafb8dc7ea7a3493c322c3126155bed5be8cbe3e
metaphorsbewithyou <ariane.morton@gmail.com>
2026-04-17 14:38:50 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -53,3 +53,7 @@ export default function App() {
     </div>
   );
 }
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
